337850b1eb880b15847f9f0b319a68ba82106d13
youneedanemail <youneedanemail.com>
2024-03-08 18:38:15 -0700

    last videos

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,79 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  const blah = 'hi';
+// define interface
+interface DanceProps {
+  name: string;
+  firstClassTaught: string;
+  courseProgression: number;
+  classInstructor: string;
+}
 
+// json object
+const BallroomDances = [
+  {
+    name: 'Waltz',
+    firstClassTaught: 'Bronze',
+    courseProgression: 1,
+    classInstructor: 'Adam Benson',
+  },
+  {
+    name: 'Tango',
+    firstClassTaught: 'Silver',
+    courseProgression: 2,
+    classInstructor: 'Ryan Podaouski',
+  },
+  {
+    name: 'Quickstep',
+    firstClassTaught: 'Bronze',
+    courseProgression: 1,
+    classInstructor: 'Adam Benson',
+  },
+  {
+    name: 'foxtrot',
+    firstClassTaught: 'Gold',
+    courseProgression: 3,
+    classInstructor: 'Natalie Shultz',
+  },
+];
+
+function Welcome() {
+  return <h1>Standard Ballroom Dances</h1>;
+}
+
+// create class component     interface name goes in <>
+class Dance extends React.Component<DanceProps> {
+  // calls return statement
+  render() {
+    const oneDance = this.props;
+
+    return (
+      <div>
+        <img />
+        <h2>{oneDance.name}</h2>
+        <h2>Level: {oneDance.firstClassTaught}</h2>
+        <h2>Class Order: {oneDance.courseProgression}</h2>
+        <h2>Teacher: {oneDance.classInstructor}</h2>
+      </div>
+    );
+  }
+}
+
+function StandardList() {
+  return (
+    <div>
+      {BallroomDances.map((DanceNumber) => (
+        <Dance {...DanceNumber} />
+      ))}
+    </div>
+  );
+}
+
+function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Kimberly was here
-        </a>
-        <>Change to save to github</>
-      </header>
+      <Welcome />
+      <StandardList />
     </div>
   );
 }
